18f4df132349ac49e96c291a165e8088488d7675
Christian <citypowah93@gmail.com>
2021-10-17 20:29:24 +0200

    cambios en script, html y css para ayudar con el js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,10 @@
 const openQuestions = () =>{
     document.getElementById('start').addEventListener('click',() =>{
+        document.getElementById('div_questions').style.display = 'default'
         document.getElementById('div_enter').style.display = 'none'
-        document.getElementById('div_questions').style.display = 'inherit';
     })
 }
+openQuestions()
 document.getElementById('bottom_index').addEventListener('click', () => nextQuestion())
 let respuestas = []
 
@@ -11,17 +12,24 @@ const endpoint = 'https://opentdb.com/api.php?amount=10&difficulty=medium&type=m
 const getInfo = async () =>{
     const data = await fetch(endpoint)
     const json = await data.json()
-    console.log(json.results[0].incorrect_answers)
+    // console.log(json.results[0].incorrect_answers)
     let preguntas = []
     let respuestas = []
     for(let i = 0; i < json.results.length ; i++){
        preguntas.push(json.results[i].question)
        respuestas.push(json.results[i].incorrect_answers)
-       respuestas.push(json.results[i].correct_answer)
+       respuestas[i].push(json.results[i].correct_answer)
     }
     
-    console.log(respuestas)
+    return infoAPI = {respuestas,preguntas}
 }
 getInfo()
-//     .then(data =>
-//         console.log(data))
+    .then(infoAPI =>{
+        document.getElementById('question').innerText = infoAPI.preguntas[1]
+        document.getElementById('q1').innerText = infoAPI.respuestas[0][0]
+        document.getElementById('q2').innerText = infoAPI.respuestas[0][1]
+        document.getElementById('q3').innerText = infoAPI.respuestas[0][2]
+        document.getElementById('q4').innerText = infoAPI.respuestas[0][3]
+        // console.log(infoAPI.respuestas[1])
+    }
+        )
