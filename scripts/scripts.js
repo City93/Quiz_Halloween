278aaab8acdaf906723993937c3989a4b62e32f9
Christian <c.ciudadparedes@gmail.com>
2021-10-27 16:30:13 +0200

    comentarios y logaritmo para desordenar array de respuestas

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,9 @@
 // Event listener que cambia el display de intro y preguntas para simular SPA
-localStorage.setItem('score', '0')
+
 const openQuestions = () =>{
     document.getElementById('start').addEventListener('click',() =>{
         document.getElementById('div_enter').style.display = 'none'
         document.getElementById('div_questions').style.display = 'inherit'
-        document.getElementById('main').style.backgroundImage = 'url("../assets/images/calabaza.jpg")';
     })
 }
 openQuestions()
@@ -26,40 +25,68 @@ const getInfo = async () =>{
         respuestas[i].push(json.results[i].correct_answer)
         correctas.push(json.results[i].correct_answer)
     }
-    
-    return infoAPI = {respuestas,preguntas, correctas}
+    // Del fetch sacamos un objeto con 3 elementos (respuestas (son 10 elementos con 4 respuestas en cada uno), preguntas y un array con las respuestas correctas.
+    return infoAPI = {respuestas,preguntas,correctas}
 }
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
 //Impresion de respuestas y preguntas
+
 let answerUser = []
-let score = 0
 getInfo()
 .then(infoAPI =>{
         // console.log(infoAPI)
+        let respuestas = infoAPI.respuestas
+        shuffle(arr);
         let contador = 0;
         for(let i = 1; i < 20; i++){
+            //Primero hay que establecer una impresion de la primera pregunta y respuestas
+            shuffle(infoAPI.respuestas[0])
             document.getElementById('question').innerHTML = infoAPI.preguntas[0]
             document.getElementById('q1').innerHTML = infoAPI.respuestas[0][0]
             document.getElementById('q2').innerHTML = infoAPI.respuestas[0][1]
             document.getElementById('q3').innerHTML = infoAPI.respuestas[0][2]
             document.getElementById('q4').innerHTML = infoAPI.respuestas[0][3]
             document.getElementById(`q${i}`).addEventListener('click', () =>{
+            // Este codigo llena el array de respuestas de usuario con el valor que ha clickado para poder comparar más adelante
                 answerUser.push(document.getElementById(`q${i}`).innerHTML)
-                // console.log(infoAPI.correctas)
+            // A continuación vamos imprimiendo cada pregunta sacando la informacion de la API con un contador que itera entre objetos.
                 if(contador < 9){
+            // Cada vez que el usuario clicka en una respuesta suma al contador para que aparezcan la siguiente pregunta y respuestas
                     contador++
+                    shuffle(infoAPI.respuestas[contador])
                 document.getElementById('question').innerHTML = infoAPI.preguntas[contador]
                 document.getElementById('q1').innerHTML = infoAPI.respuestas[contador][0]
                 document.getElementById('q2').innerHTML = infoAPI.respuestas[contador][1]
                 document.getElementById('q3').innerHTML = infoAPI.respuestas[contador][2]
                 document.getElementById('q4').innerHTML = infoAPI.respuestas[contador][3]
-                console.log(contador)
+            //Cuando llega el contador a 9 (numero de preguntas - 1 ) el listener provoca que desaparezca la ventana de preguntas y devuelva los resultados
                 } else{
                     document.getElementById('div_questions').style.display = 'none'
                     document.getElementById('div_results').style.display = 'inherit'
                     console.log(infoAPI.correctas)
                     console.log(answerUser)
-                    localStorage.setItem('score', JSON.stringify({
+
+                    // Despues de realizar el quiz este codigo guarda el usuario y su resultado
+                    // El resultado se obtiene comparando 2 arrays (Respuestas correctas y respuestas del usuario)
+                    // Con los dos arrays se obtiene un array de true's y false's del que se filtra para sacar la longuitud el array con solo true's
+                    localStorage.setItem('Isa', JSON.stringify({
                         score: infoAPI.correctas.map((el,i) => {
                             if(el == answerUser[i]){
                                 return true
@@ -68,32 +95,12 @@ getInfo()
                             }
                         }).filter(Boolean).length
                     }))
-                    infoAPI.correctas.map((el,i) => {
-                        if(el == answerUser[i]){
-                            return true
-                        } else {
-                            return false
-                        }
-                    }).filter(Boolean).length
                 }
-                document.getElementById('question').addEventListener('click', () =>{
-                })
-
             })     
         }
         // console.log(infoAPI.respuestas[1])
-    }
-        )
+    })
 
-
-console.log('esto es score-->' + score)
-
-// let array1 = ['cosa1', 'cosa2', 'cosa3']
-// let array2 = ['cosa1', 'cosa2', 'cosa3']
-// let resultado = array1.map((el,i) => {
-//     if(el == array2[i]){
-//         return true
-//     } else {
-//         return false
-//     }
-// }).filter(Boolean).length
+    var arr = [2, 11, 37, 42];
+shuffle(arr);
+console.log(arr);
