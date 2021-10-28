910bcca258da6ed6f095f11434f8c334cc507832
IsaGuapo <isabela.guapo@gmail.com>
2021-10-28 13:36:27 +0200

    Cambio estilo grafica

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@
 const openQuestions = () =>{
     document.getElementById('start').addEventListener('click',() =>{
         document.getElementById('div_enter').style.display = 'none'
-        document.getElementById('div_questions').style.display = 'inherit'
+        document.getElementById('div_questions').style.display = 'flex'
         document.getElementById('main').style.backgroundImage = 'url("../assets/images/calabaza.jpg")';
     })
 }
